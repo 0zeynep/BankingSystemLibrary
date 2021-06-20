85a2e600dd23f9cdf1a86c21de16438c1ef547e0
hanzig97 <marcinsluzalek@gmail.com>
2021-06-20 13:12:33 +0200

    BankingSystem class created with Singleton implementation

---
+++ BankingSystem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingSystemLibrary
+{
+    class BankingSystem
+    {
+        static BankingSystem myBankingSystem;
+        protected BankingSystem()
+        {
+        }
+        public static BankingSystem Instance()
+        {
+            if (myBankingSystem == null)
+            {
+                myBankingSystem = new BankingSystem();
+            }
+            return myBankingSystem;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingSystemLibrary
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Constructor is protected -- cannot use new
+            BankingSystem myBankingSystem = BankingSystem.Instance();
+
+            Console.ReadKey();
+        }
+    }
+}
